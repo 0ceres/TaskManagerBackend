f8adcee8ddac61d10ee6927040288dd081455fec
MECIOSCOQUEROR\ocere <rceres>
2026-02-26 11:52:11 -0400

    Add task repository interface to domain

--- TaskManager.Domain/Interfaces/ITaskRepository.cs
+++ TaskManager.Domain/Interfaces/ITaskRepository.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskManager.Domain.Entities;
 
-namespace TaskManager.Domain.Interfaces
+namespace TaskManager.Domain.Interfaces;
+
+public interface ITaskRepository
 {
-    internal interface ITaskRepository
-    {
-    }
+    Task<TaskItem?> GetByIdAsync(Guid id);
+    Task<IReadOnlyList<TaskItem>> GetAllAsync();
+    Task AddAsync(TaskItem task);
+    Task UpdateAsync(TaskItem task);
+    Task DeleteAsync(TaskItem task);
 }
